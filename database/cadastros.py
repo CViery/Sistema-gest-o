1d483245d74e38b40459d5f01545797263e0e9e7
Cristian Viery <viery.guimaraes01@gmail.com>
2024-07-08 21:09:02 -0300

    arrumei essa merda

--- database/cadastros.py
+++ database/cadastros.py
@@ -1,15 +1,31 @@
-from database import conection
+import sqlite3
+import threading
 
-class CadastrosDB : 
-  def __init__(self) -> None:
-    self.db = conection.Database()
-    self.cursor = self.db.cursor
-    
-  def cadastrar_produto(self,dados):
-    try:
-      query="INSERT INTO produtos VALUES(?,?,?,?,?,?,?)"
-      self.cursor.execute(query,(dados["id"],dados["codigo"],dados["nome"],dados["categoria"],dados["valor_compra"],dados["estoque_inicial"],dados["estoque_min"]))
-      self.db.commit()
-      print("Salvo")
-    except Exception as e:
-      print(e)
+class CadastrosDB:
+    def __init__(self):
+        self.local = threading.local()
+
+    def get_conn(self):
+        if not hasattr(self.local, 'conn'):
+            self.local.conn = sqlite3.connect('database.db')
+        return self.local.conn
+
+    def cadastrar_produto(self, dados):
+        try:
+            conn = self.get_conn()
+            cursor = conn.cursor()
+            id = dados["id"]
+            codigo = dados["codigo"]
+            nome = dados["nome"]
+            categoria = dados["categoria"]
+            valor = dados["valor_compra"]
+            estoque_inicial = dados["estoque_inicial"]
+            estoque_min = dados["estoque_min"]
+            query = "INSERT INTO produtos VALUES (?, ?, ?, ?, ?, ?, ?)"
+            cursor.execute(query, (id, codigo, nome, categoria, valor, estoque_inicial, estoque_min))
+            conn.commit()
+            print("Salvo")
+        except Exception as e:
+            print(e)
+        finally:
+            cursor.close()
--- database/conection.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-
-class Database: 
-  def __init__(self) -> None:
-    self.conn = sqlite3.connect('database.db')
-    self.cursor = self.conn.cursor()
